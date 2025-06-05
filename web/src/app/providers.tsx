7b24b295aac0e305d455c9673ddba1b277b2e310
s3rxio <cergotabagua222@gmail.com>
2025-06-05 13:37:50 +0300

    feat(web): init web app

---
+++ web/src/app/providers.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+
+export const Providers: FC = () => {
+    return <BrowserRouter></BrowserRouter>;
+}
+
+export default Providers;
